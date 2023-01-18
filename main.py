a94d4a9a2c1ef14ce3f0fde373f6ef4761c0966e
Leo Martinez <123003509+LeoMartinezTAMUK@users.noreply.github.com>
2023-01-18 11:23:14 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+""" Leo Martinez III, K00475695, Assignment 6 """
+
+S1 = [3, 9, 14, 18]
+S2 = [2, 5, 6, 15, 56]
+S = []
+
+S = S1 + S2
+def merge(S1, S2, S):
+  # Merge S1 and S2 into properly sized list S
+  i = j = 0
+  while i+j < len(S):
+    if j == len(S2) or (i<len(S1) and S1[i]<S2[j]):
+      S[i+j] = S1[i]
+      i = i+1
+    else:
+      S[i+j] = S2[j]
+      j = j+1
+  
+merge(S1, S2, S)
+print(S)
